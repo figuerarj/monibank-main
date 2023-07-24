70c8a482cde1664a10dc34f834a40ccd6ee51977
Rodrigo Santos <79957513+figuerarj@users.noreply.github.com>
2023-07-24 11:26:37 -0300

    link para validacao da idade

--- js/script.js
+++ js/script.js
@@ -1,16 +1,29 @@
 import ehUmCPF from "./valida-cpf.js"
+import ehMaiorDeIdade from "./valida-idade.js"
 
 const camposDoFormulario = document.querySelectorAll("[required]")
 
 //para cada campo que ele reconhecer que tem o required dentro, entao será realizado...
 camposDoFormulario.forEach((campo) => {
     // evento blur é quando tiramos o foco do input. No caso quando tiramos o foco do campo entao o verificaCampo é acionado.
-    campo.addEventListener("blur", () => verificaCampo(campo))
+    campo.addEventListener("blur", () => verificaCampo(campo));
+    //previne o js de enviar algum erro sem o nosso consentimento.
+    campo.addEventListener("invalid", evento => evento.preventDefault())
 
 })
 
 function verificaCampo(campo){
+    // se o campo name estiver com o valor cpf = true ou se o campo tiver menos ou igual a 11 numeros .
+    // entao a funcao ehUmCPF é executada.
     if(campo.name == "cpf" && campo.value.length >= 11){
         ehUmCPF(campo);
     }
+    // se o campo name estiver com aniversario = true ou se o campo value for diferente de vazio = true. 
+    // entao ele ira chamar a funcao eh maior idade.
+    if(campo.name == "aniversario" && campo.value != ""){
+        ehMaiorDeIdade(campo);
+    }
+    // .validity - verifica automaticamente uma serie de possiveis erros e retorna verdd ou falso
+
+    console.log(campo.validity);
 }

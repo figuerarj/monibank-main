23f14dc64956fecd15ba72658e6add6ac0f4b3ab
Rodrigo Santos <79957513+figuerarj@users.noreply.github.com>
2023-07-24 11:46:52 -0300

    inserido tiposDeErro e Mensagens para os Erros

--- js/script.js
+++ js/script.js
@@ -12,6 +12,51 @@ camposDoFormulario.forEach((campo) => {
 
 })
 
+const tiposDeErro = [
+    // quando nao tem nada dentro do campo, campo vazio.
+    'valueMissing',
+    // quando nao combina o elemento com o tipo que estamos preenchendo.
+    'typeMismatch',
+    // o pattern nao é igual ao padrao
+    'patterMismatch',
+    // texto menor que o minlength
+    'tooShort',
+    // erros customizados
+    'customError'
+]
+
+// mensagens customizadas
+const mensagens = {
+    nome: {
+        valueMissing: "O campo de nome não pode estar vazio.",
+        patternMismatch: "Por favor, preencha um nome válido.",
+        tooShort: "Por favor, preencha um nome válido."
+    },
+    email: {
+        valueMissing: "O campo de e-mail não pode estar vazio.",
+        typeMismatch: "Por favor, preencha um email válido.",
+        tooShort: "Por favor, preencha um e-mail válido."
+    },
+    rg: {
+        valueMissing: "O campo de RG não pode estar vazio.",
+        patternMismatch: "Por favor, preencha um RG válido.",
+        tooShort: "O campo de RG não tem caractéres suficientes."
+    },
+    cpf: {
+        valueMissing: 'O campo de CPF não pode estar vazio.',
+        patternMismatch: "Por favor, preencha um CPF válido.",
+        customError: "O CPF digitado não existe.",
+        tooShort: "O campo de CPF não tem caractéres suficientes."
+    },
+    aniversario: {
+        valueMissing: 'O campo de data de nascimento não pode estar vazio.',
+        customError: 'Você deve ser maior que 18 anos para se cadastrar.'
+    },
+    termos: {
+        valueMissing: 'Você deve aceitar nossos termos antes de continuar.',
+    }
+}
+
 function verificaCampo(campo){
     // se o campo name estiver com o valor cpf = true ou se o campo tiver menos ou igual a 11 numeros .
     // entao a funcao ehUmCPF é executada.
